9e702d6abd9d4390c3b2abb57b3cb662513bc2d4
Brahim EL Mahkour <Brahim EL Mahkour>
2023-06-27 07:56:17 -0700

    all at once

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""defines a square object"""
+
+
+class Square:
+    """Creates an empty square class"""
+    pass
---
+++ 0x06-python-classes/README.md
@@ -0,0 +1,1 @@
+this Directory created in order to make things in python-classes more understandable
